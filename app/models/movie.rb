8c3ed1a9adb22259cbb255bbe2cdaaa14f40799e
tRav3r <tRav3r@gmail.com>
2014-06-02 03:03:07 +0300

    Add fix, stylesheets for simple_form.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,8 @@ class Movie < ActiveRecord::Base
   RATINGS = %w(G PG PG-13 R)
  
   validates :title, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, inclusion: RATINGS
+
 
   scope :with_ratings, ->(ratings) { where('rating in (?)', ratings) unless ratings.blank? }
 end
